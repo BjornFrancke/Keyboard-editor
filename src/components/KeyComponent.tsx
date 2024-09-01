3b39d13b8e11ed0906a119259cc36b71d727ef29
Bjorn Francke <bjorn@welcomebob.com>
2024-09-01 22:48:07 +0200

    feature: "ghost" keys now get a selected indicator

--- src/components/KeyComponent.tsx
+++ src/components/KeyComponent.tsx
@@ -8,19 +8,21 @@ export const KeyComponent = ({letter, width, location, isSelected}: {
     const styles = {
         transform: `translate(${location?.x || 0}px, ${location?.y || 0}px)`,
         width: width ? `${width * 2.5}rem` : "2.5rem",
-        borderColor: isSelected ? "#ef4444" : "rgb(107,114,128)",
+        borderColor: isSelected ? "#ef4444" : "",
     }
 
     if (letter === "") {
 
         return (
-            <div className={"size-10 flex justify-center bg-blue-500  text-gray-700 rounded select-none"}
-                 style={styles}>-</div>
+            <div
+                className={"size-10 flex justify-center bg-blue-500 border border-blue-500 text-gray-700 rounded select-none"}
+                style={styles}>-</div>
         )
     }
 
 
-    return (<div
+    return (
+        <div
             className={"h-[40px] flex justify-center shadow bg-white border-gray-500 border rounded select-none cursor-pointer"}
             style={styles}>{letter}</div>
     )
